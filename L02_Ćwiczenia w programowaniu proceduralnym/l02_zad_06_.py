9ee4199661f98a76e0b89315373c2110598a823e
marianstrilets <marianstrilets@gmail.com>
2022-12-02 22:49:07 +0100

    Delete l02_zad_06_.py

--- L02_Ćwiczenia w programowaniu proceduralnym/l02_zad_06_.py
+++
@@ -1,8 +0,0 @@
-
-'''
-Utwórz poniższą macierz, wykorzystuj¡c jedną linijkę kodu 
-(dwie pętlę for inicjalizujące listę): [[1, 4, 7], [1, 4, 7], [1, 4, 7]]
-'''
-
-l = [ [ i for i in range(1,8,3)] for j in range(3) ]
-print(l)
