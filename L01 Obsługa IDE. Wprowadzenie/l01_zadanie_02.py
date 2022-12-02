c8caa179d799b3c3ac4e3214b6d88833d4bea39c
marianstrilets <marianstrilets@gmail.com>
2022-12-02 21:56:06 +0100

    Create l01_zadanie_02.py

---
+++ L01 Obsługa IDE. Wprowadzenie/l01_zadanie_02.py
@@ -0,0 +1,8 @@
+#Laboratorium 1
+#zadanie 2
+
+adress = input("Podaj adress zamieszkania: ")
+postcode = input("Podaj kod pocztowy: ")
+city = input("Podaj miasto: ")
+
+print(f"Twój adress: {adress}", f"Twój kod pocztowy: {postcode}", f"Twoje miasto: {city}", sep="\n", end="\n============================================")
