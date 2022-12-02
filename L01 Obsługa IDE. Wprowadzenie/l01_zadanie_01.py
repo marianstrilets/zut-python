798bb173ac9efe7d971772a6eda69a55d44b6ca1
marianstrilets <marianstrilets@gmail.com>
2022-12-02 21:56:09 +0100

    Create l01_zadanie_01.py

---
+++ L01 Obsługa IDE. Wprowadzenie/l01_zadanie_01.py
@@ -0,0 +1,8 @@
+#Laboratorium 1
+#zadanie 1
+
+faculty = input("Podaj kierunek studiow: ")
+year = int(input("Podaj rok studiow: "))
+average = int(input("Średnia ocena: "))
+
+print(f"Kierunek: {faculty}", f"Rok studiów: {year}", f"Średnia: {average}", sep=", ")
